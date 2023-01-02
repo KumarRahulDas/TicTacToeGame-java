5dcea18fa0a9e0e0e8d1efc9122963b3dcff09b5
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-01-02 02:47:10 -0800

    Uc2 Commited

--- src/packagecom/blz/TicTacToeGame.java
+++ src/packagecom/blz/TicTacToeGame.java
@@ -1,7 +1,37 @@
 package com.blz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    public static char inputUser;
+    public static char inputC;
+    public static char[] board = new char[10];
+
     public static void main(String[] args) {
-        System.out.println("welcome to tic tac toe");
+
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+        char inputUser = input();
+        if (inputUser == 'X') {
+            inputC = 'O';
+        } else {
+            inputC = 'X';
+        }TicTacToeGame.showBoard();
     }
-} 
+
+    public static char input() {
+        System.out.println("Please enter your input in X / O");
+        Scanner in = new Scanner(System.in);
+        char inputU = in.next().charAt(0);
+        return inputU;
+    }
+
+    public static void showBoard() {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("---------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("---------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
+}
