6ceaa3f21ef378233e974bd765bebec9a73fe17d
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-01-02 02:50:21 -0800

    Newly Commited

--- src/packagecom/blz/TicTacToeGame.java
+++ src/packagecom/blz/TicTacToeGame.java
@@ -1,7 +1,52 @@
 package com.blz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    public static char inputUser;
+    public static char inputC;
+    public static char[] board = new char[10];
+
     public static void main(String[] args) {
-        System.out.println("welcome to tic tac toe");
+
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+        inputUser = input();
+        if (inputUser == 'X') {
+            inputC = 'O';
+        } else {
+            inputC = 'X';
+        }
+        TicTacToeGame.showBoard();
+        TicTacToeGame.uSelection();
+        TicTacToeGame.showBoard();
     }
-} 
+
+    public static char input() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your input in X / O");
+        char inputU = in.next().charAt(0);
+        return inputU;
+    }
+
+    public static void showBoard() {
+        System.out.println("TicTacToe Board");
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("---------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("---------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
+
+    public static void uSelection() {
+        System.out.println("Please enter the index from 1 to 9 to make the move");
+        Scanner in1 = new Scanner(System.in);
+        int select = in1.nextInt();
+        for (int i = 1; i < 10; i++) {
+            if (board[select] == ' ') {
+                board[select] = inputUser;
+            }
+        }
+    }
+}
